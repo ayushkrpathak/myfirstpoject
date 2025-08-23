c1f504feae73ff30f2eb7e30db066dd4741c3b67
ayushkrpathak <ayushkrpathak21@gmail.com>
2025-08-23 10:17:10 +0530

    commit changes

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import blog_index,get_posts,create_post
+
+urlpatterns = [
+    path('',blog_index),
+    path('posts/', get_posts),
+path('posts/new/', create_post),
+]
